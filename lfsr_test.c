785de4d28b2c92bfe0f68c6cf0069fb691e05570
bhadai98 <billel.hadj@etu.univ-paris-diderot.fr>
2018-11-27 16:01:23 +0100

    Mise a jour du main.c

--- lfsr_test.c
+++ lfsr_test.c
@@ -7,15 +7,22 @@
 int main (int argc, char* argv[]){
 printf("Début de programme bien défini\n");
   container_t *cont = malloc(sizeof(container_t)) ;
+  printf("Déclaration du pointeur sur un container fait avec succes\n");
   lfsr_t *lfsr1 =malloc(sizeof(lfsr_t));
+  printf("Déclaration du pointeur sur un lfsr fait avec succes\n");
   lfsr_container_init(cont, 0x80,0x71);
   lfsr_init(lfsr1,0,8,cont) ;
-  printf("%lx \n", lfsr1->cont->regist);
-  for(int i = 0; i< 20 ; i++){
+  printf("Initialisation du container et du LFSR avec les paramètre de l'exemple.txt fait avec succes\n");
+  printf("Affichage du registre : %lx \n", lfsr1->cont->regist);
+  printf("Affichage du polynome : %lx \n", lfsr1->cont->poly);
+  printf("Affichage de toutes les valeurs de l'incrémentation du LFSR de l'exemple.txt\n");
+  for(int i = 0; i< 255 ; i++){
       lfsr_inc(lfsr1);
-      printf("%lx \n", lfsr1->cont->regist);
+      printf("Incrémentation n° : %4d          valeur du registre : %4lx \n",i+1, lfsr1->cont->regist);
   }
-  printf("\n%d\n",lfsr_period(lfsr1));
 
+  printf("Test de la fonction de période pour l'exemple du cours : %d\n",lfsr_period(lfsr1));
+
+  printf("Fin des tests pour les fonctions :\n-lfsr_container_init()\n-lfsr_init()\n-lfsr_inc()\n-lfsr_period()\n");
   return 0;
 }
